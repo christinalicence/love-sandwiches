56ca2186c26efb82f0456e612b80673556e22fec
christinalicence <christinalicence@gmail.com>
2025-09-10 10:36:57 +0100

    create dictionary to make feedback to user about how many sandiwches to make

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -78,7 +78,6 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
@@ -103,6 +102,33 @@ def get_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
+def get_stock_values(data):
+    """
+    Create a dictionary of sandwich stock values.
+    Keys are headings from the 'stock' worksheet (first row),
+    Values are from the 'data' parameter.
+    """
+    headings = SHEET.worksheet("stock").row_values(1)
+    stock_dict = {key: value for key, value in zip(headings, data)}
+    return stock_dict
+
+
 def main():
     """
     Run all program functions
@@ -112,9 +138,13 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
+    print("Recommended stock values for next market:")
+    print(stock_values)
 
 
 print("Welcome to Love Sandwiches Data Automation")
-# main()
-
-sales_columns = get_last_5_entries_sales()
+main()
